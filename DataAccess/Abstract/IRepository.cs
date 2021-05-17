0c3e4cb939bb96fb11c41ee9831ba44b58d68f75
DESKTOP-UP0D5VP\aysek <ayse.klc.95@gmail.com>
2021-05-17 23:35:25 +0300

    added db-migration added abstract classes

---
+++ DataAccess/Abstract/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Abstract
+{
+   public interface IRepository<T>
+    {
+        List<T> List();
+
+        void Insert(T p);
+        void Update(T p);
+        void Delete(T p);
+    }
+}
---
+++ DataAccess/Concrete/Repositories/CategoryRepository.cs
@@ -0,0 +1,38 @@
+﻿using DataAccess.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete.Repositories
+{
+    public class CategoryRepository : IRepository<Category>
+    {
+        MvcContext c = new MvcContext();
+        DbSet<Category> _object;
+        public void Delete(Category p)
+        {
+            _object.Remove(p);
+            c.SaveChanges();
+        }
+
+        public void Insert(Category p)
+        {
+            _object.Add(p);
+            c.SaveChanges();
+        }
+
+        public List<Category> List()
+        {
+            return _object.ToList();
+        }
+
+        public void Update(Category p)
+        {
+            c.SaveChanges();
+        }
+    }
+}
