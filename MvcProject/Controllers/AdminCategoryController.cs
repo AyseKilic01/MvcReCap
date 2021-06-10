a0598c4d4aca41df0dbf039b9c87515e8f1f0b6b
DESKTOP-UP0D5VP\aysek <ayse.klc.95@gmail.com>
2021-06-10 22:27:56 +0300

    edited AdminCategoryController, added indexadmin added addcategoryadmin

--- MvcProject/Controllers/AdminCategoryController.cs
+++ MvcProject/Controllers/AdminCategoryController.cs
@@ -1,7 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using BusinessLayer.Concrete;
+using BusinessLayer.ValidationRules;
+using DataAccess.Concrete.EntityFramework;
+using EntityLayer.Concrete;
+using FluentValidation.Results;
 using System.Web.Mvc;
 
 namespace MvcProject.Controllers
@@ -9,8 +10,36 @@ namespace MvcProject.Controllers
     public class AdminCategoryController : Controller
     {
         // GET: AdminCategory
+        CategoryManager category = new CategoryManager(new EfCategoryDAL());
+        CategoryValidator validate = new CategoryValidator();
+
         public ActionResult Index()
         {
+            var categoryvalues = category.GetAllBL();
+            return View(categoryvalues);
+        }
+        [HttpGet]
+        public ActionResult AddCategory()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult AddCategory(Category p)
+        {
+
+            ValidationResult validation = validate.Validate(p);
+            if (validation.IsValid)
+            {
+                category.Add(p);
+                return RedirectToAction("GetCategoryList");
+            }
+            else
+            {
+                foreach (var i in validation.Errors)
+                {
+                    ModelState.AddModelError(i.PropertyName, i.ErrorMessage);
+                }
+            }
             return View();
         }
     }
